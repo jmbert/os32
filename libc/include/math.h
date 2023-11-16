037af3939f1bb1455dd4f627f2594cf49d354bf6
jmbert <joe2157b@gmail.com>
2023-11-16 16:13:24 +0000

    Add basic comparison and math.h

---
+++ libc/include/math.h
@@ -0,0 +1,10 @@
+#ifndef _LIBC_MATH_H
+#define _LIBC_MATH_H
+
+#include <sys/cdefs.h>
+
+
+int max(int x, int y);
+int min(int x, int y);
+
+#endif
---
+++ libc/include/math/comp.c
@@ -0,0 +1,10 @@
+#include <math.h>
+
+int max(int x, int y)
+{
+    return (x > y) ? x : y;
+}
+int min(int x, int y)
+{
+    return (x > y) ? y : x;
+}
