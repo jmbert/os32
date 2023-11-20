d4136425a52d03092e96f041e44ce7985aa9de14
jmbert <joe2157b@gmail.com>
2023-11-20 16:36:56 +0000

    Fixed mmap issues with hitting max entries

--- kernel/include/memory.h
+++ kernel/include/memory.h
@@ -17,7 +17,7 @@ struct memmap
     uint64_t type;
 };
 
-#define MAX_MAP_ENTRIES 0x1000
+#define MAX_MAP_ENTRIES 0x10000
 
 #define RANGES_CONFLICT(addr1, len1, addr2, len2)   (((addr1 > addr2) && addr1 < (addr2 + len2)) ||\
                                                     ((addr2 > addr1) && addr2 < (addr1 + len1)) ||\
@@ -36,6 +36,13 @@ extern uint64_t memmaplen;
     }\
     memmap[position] = map\
 
+#define MOVE_DOWN(index)\
+    for (int __index = index; __index < memmaplen-1; __index++)\
+    {\
+        memmap[__index] = memmap[__index+1];\
+    }\
+    memmaplen--;\
+
 enum
 {
     MEMMAP_MEM_AVAILABLE = 1,
@@ -51,7 +58,7 @@ enum
     MEMMAP_GRUB_MODULES,
 };
 
-#define NEWUSERMAP(addr, len) _new_map(addr, len, MEMMAP_MEM_USER) == NULL);
+#define NEWUSERMAP(addr, len) _new_map(addr, len, MEMMAP_MEM_USER);
 #define NEWPAGINGMAP(addr, len) _new_map(addr, len, MEMMAP_MEM_PAGEFRAMES);
 #define NEWKERNELMAP(addr, len) _new_map(addr, len, MEMMAP_MEM_KERNEL);
 
@@ -60,5 +67,8 @@ int _new_map(uint64_t addr, uint64_t len, uint8_t type);
 void reserve_mem(multiboot_info_t *mbinfo);
 
 uint64_t alloc_mem(uint64_t memsize, uint8_t type, uint64_t align);
+
+
+void print_maps();
 
 #endif
