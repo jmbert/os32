c6ebd943896f07eb6f104b6f79cdc93aaa0789d1
jmbert <joe2157b@gmail.com>
2023-11-20 16:36:06 +0000

    Optimised memcpy

--- libc/string/memcpy.c
+++ libc/string/memcpy.c
@@ -1,18 +1,22 @@
 #include <string.h>
 
-void *memcpy(void *destination, void *source, size_t num)
+#include <stdint.h>
+
+void *memcpy(void *dest, void *src, size_t n)
 {
-#ifdef _IN_KERNEL
+  	uint32_t num_dwords = n/4;
+  	uint32_t num_bytes = n%4;
+  	uint32_t *dest32 = (uint32_t*)dest;
+  	uint32_t *src32 = (uint32_t*)src;
+  	uint8_t *dest8 = ((uint8_t*)dest)+num_dwords*4;
+  	uint8_t *src8 = ((uint8_t*)src)+num_dwords*4;
+  	uint32_t i;
 
-	char* d = destination;
-	char* s = source;
-	for (int i = 0; i < num; i++) {
-		d[i] = s[i];
-	}
-	return destination;
-
-#else
-
-
-#endif
+  	for (i=0;i<num_dwords;i++) {
+  	  dest32[i] = src32[i];
+  	}
+  	for (i=0;i<num_bytes;i++) {
+  	  dest8[i] = src8[i];
+  	}
+  	return dest;
 }
